7ba72162213d30b35602cf453aaf456bedac8203
hegeda <35102729+hegeda@users.noreply.github.com>
2018-06-07 15:24:57 +0200

    update2

--- blackjack.py
+++ blackjack.py
@@ -7,29 +7,39 @@ kartyak = {'2': 2,'3': 3,'4': 4,'5': 5,'6': 6,'7': 7,'8': 8,'9': 9,'10': 10,'J':
 vege = False
 
 class Jatekos(object):
-	def __init__(self,penz,lapok=[]):
-		self.penz=penz
-		self.lapok=lapok
-	
-	def megall(self,stop):
-		self.stop = stop
-		stop = False
-		return stop
-	
+        def __init__(self,penz,lapok=[]):
+                self.penz=penz
+                self.lapok=lapok
+
 def ertekel():
-	global vege
-	if sum(jatekos.lapok) > 21:
-		print 'Veszettél! A lapok összege: ', sum(jatekos.lapok)
-		vege = True
-	else:
-		jatek()
+        global vege
+        osszeg = sum(jatekos.lapok)
+        if osszeg > 21:
+                print 'Veszettél! A lapok összege: ', osszeg
+                vege = True
+        elif osszeg <= 21:
+                print 'Nyertél!', osszeg
+                vege = True
+
+
+def lapotker():
+        re = raw_input("Kér lapot?: (i/n)")
+        if re == 'i':
+                jatek()
+        else:
+                ertekel()
 
 def jatek():
-	lap = random.choice(kartyak.items())
-	jatekos.lapok.append(lap[1])
-	print lap[0]
-	ertekel()	
+        global vege
+        lap = random.choice(kartyak.items())
+        jatekos.lapok.append(lap[1])
+        print lap[0]
+        if sum(jatekos.lapok) > 21:
+                print 'Veszettél! A lapok összege: ', sum(jatekos.lapok)
+                vege = True
+
+        lapotker()
 
 while not vege:
-	jatekos = Jatekos(1000)
-	jatek()
+        jatekos = Jatekos(1000)
+        jatek()
