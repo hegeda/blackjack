df8818c3d146c790218973ab14b2e33af7d61e5d
hegeda <35102729+hegeda@users.noreply.github.com>
2018-06-07 13:59:13 +0200

    init verzió

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python
 # coding=utf-8
-
 import os
+import random
 
-f
+lapok = {'2': 2,'3': 3,'4': 4,'5': 5,'6': 6,'7': 7,'8': 8,'9': 9,'10': 10,'J': 10,'D': 10,'K': 10,'A': 11}
+vege = False
+osszeg = 0
 
+class Jatekos(object):
+	def __init__(self,penz,lapok={}):
+		self.penz=penz
+		self.lapok=lapok
+	
+	def megall(self,stop):
+		self.stop = stop
+		stop = False
+		return stop
+	
+def ertekel():
+	if osszeg > 21:
+		print 'Veszettél! A lapok összege: ', osszeg 		
+	jatek()
+
+def jatek():
+	jatekos = Jatekos(1000)
+	lap = random.choice(lapok.items())
+	jatekos.lapok.append(random.choice(lap.values()))
+	print lap.keys
+ 	for i in jatekos.lapok:
+		ossz=ossz+i
+	ertekel()	
+
+while not vege:
+	jatek()
