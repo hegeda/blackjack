87294f866b7c8f8a21a3b64ad047954158b7bf85
hegeda <davidhege9@gmail.com>
2018-08-19 14:04:19 +0200

    -működő verzió osztályokkal

--- blackjack_class.py
+++ blackjack_class.py
@@ -23,8 +23,9 @@ class Jatekos(object):
         self.ertek = ertek
 
     def tetrakas(self):
+        print 'Pénz: ', self.penz
         rak = input('Add meg a tétet: (min. 100)')
-        self.tet=rak
+        self.tet = rak
         return self.tet
 
     def lapothuz(self,lap):
@@ -36,12 +37,13 @@ class Jatekos(object):
         valasz = raw_input("Kér lapot?: (i/n)")
         return valasz
 
+
 class Asztal(object):
     def __init__(self, penz, tet, josszeg, oosszeg, j_lapok=[], o_lapok=[]):
         self.penz = penz
         self.tet = tet
-        self.j_osszeg = josszeg
-        self.o_osszeg = oosszeg
+        self.josszeg = josszeg
+        self.oosszeg = oosszeg
         self.j_lapok = j_lapok
         self.o_lapok = o_lapok
 
@@ -50,50 +52,116 @@ class Asztal(object):
         print 'Pénz: ', self.penz
         print 'Tét: ', self.tet
         print 'Lapok', self.j_lapok
-        print 'összeg: ', self.j_osszeg
+        #print 'összeg: ', josszeg
 
     def o_rajzol(self):
         clear()
         print 'Pénz: ', self.penz
         print 'Tét: ', self.tet
         print 'Lapok:', self.j_lapok
-        print 'összeg:', self.j_osszeg
+        #print 'összeg:', josszeg
         print 'Osztó:', self.o_lapok
-        print 'osztóösszeg: ', self.o_osszeg
+        #print 'osztóösszeg: ', oosszeg
 
-def jatek():
-
-    if osszeg_jatekos > 21:
-        print 'Veszettél! A lapok összege: ', osszeg_jatekos
+def ertekel():
+    if jatekos.osszeg > 21:
+        print 'Veszettél! A lapok összege: ', jatekos.osszeg
+        tovabb()
         jatekvege()
-    elif 21 - osszeg_jatekos < 21 - osszeg_oszto:
+    elif oszto.osszeg > 21:
         print 'Nyertél!'
-        print 'Játékos lapjainak összege: ', sum(jatekos.lapok)
-        print 'Osztó lapjainak összege: ', sum(oszto.lapok)
+        print 'Játékos lapjainak összege: ', jatekos.osszeg
+        print 'Osztó lapjainak összege: ', oszto.osszeg
         jatekos.penz = jatekos.penz + 2 * jatekos.tet
-    else:
-        print 'Vesztettél!'
-        print 'Játékos lapjainak összege: ', sum(jatekos.lapok)
-        print 'Osztó lapjainak összege: ', sum(oszto.lapok)
+        tovabb()
         jatekvege()
+    elif 21 - jatekos.osszeg < 21 - oszto.osszeg:
+        print 'Nyertél!'
+        print 'Játékos lapjainak összege: ', jatekos.osszeg
+        print 'Osztó lapjainak összege: ', oszto.osszeg
+        jatekos.penz = jatekos.penz + 2 * jatekos.tet
+        tovabb()
+        jatekvege()
+    else:
+        print 'Veszettél! A lapok összege: ', jatekos.osszeg
+        tovabb()
+        jatekvege()
+
+def tovabb():
+    global vege
+    valasz = raw_input("--tovább|q: kilép--")
+    if valasz == 'q':
+        if jatekos.penz > 0:
+            print 'Nyeremény: ', jatekos.penz
+            vege = True
+            jatekvege()
+        else:
+            vege = True
+            jatekvege()
+
 
 def jatekvege():
     global vege
     if jatekos.penz == 0:
         vege = True
 
+def init():
+    clear()
+    jatekos.lapok = []
+    jatekos.ertek = []
+    jatekos.osszeg = 0
+    oszto.lapok = []
+    oszto.ertek = []
+    oszto.osszeg = 0
+
 
 jatekos = Jatekos(1000, 0, 0, [])
 oszto = Jatekos(0, 0, 0, [])
 kartya = Kartya()
+
+
 while not vege:
-    jatekos.tetrakas()
-    jatekos.lapothuz(kartya.lap())
+    init()
+    tet = jatekos.tetrakas()
+
+    if tet < 100:
+        print 'Minimum 100!'
+        tovabb()
+        continue
+    elif tet > jatekos.penz:
+        print 'Nincs elég pénzed!'
+        tovabb()
+        continue
+    else:
+        jatekos.penz = jatekos.penz - jatekos.tet
+        jatekos.lapothuz(kartya.lap())
+
     #asztal = Asztal(penz=jatekos.penz, tet=jatekos.tet, j_lapok=jatekos.lapok, o_lapok=oszto.lapok,j_laposszeg=sum(jatekos.lapok), o_laposszeg=sum(oszto.lapok))
-    asztal = Asztal(penz=jatekos.penz,tet=jatekos.tet, josszeg=jatekos.osszeg, oosszeg=oszto.osszeg, j_lapok=jatekos.lapok,o_lapok=oszto.lapok)
+    asztal = Asztal(penz=jatekos.penz, tet=jatekos.tet, josszeg=jatekos.osszeg, oosszeg=oszto.osszeg,
+                    j_lapok=jatekos.lapok, o_lapok=oszto.lapok)
+    #asztal.j_rajzol(jatekos.osszeg)
     asztal.j_rajzol()
-    if jatekos.lapotker() == 'n':
+    while jatekos.lapotker() == 'i':
+        jatekos.lapothuz(kartya.lap())
+        #asztal.j_rajzol(jatekos.osszeg)
+        asztal.j_rajzol()
+        if jatekos.osszeg > 21:
+            break
+
+    if jatekos.osszeg > 21:
+        ertekel()
+        continue
+
+    oszto.lapothuz(kartya.lap())
+    #asztal.o_rajzol(jatekos.osszeg, oszto.osszeg)
+    asztal.o_rajzol()
+    while oszto.osszeg <= 17:
         oszto.lapothuz(kartya.lap())
+        #asztal.o_rajzol(jatekos.osszeg,oszto.osszeg)
         asztal.o_rajzol()
+        if oszto.osszeg > 21:
+            break
+    ertekel()
+
 
 
